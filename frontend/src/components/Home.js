a55268c0dd6da0ea7d4f96ac6dd479b9f22e5d4c
Tony111719 <tonytony111719@gmail.com>
2025-04-14 22:29:11 +0200

    Create Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <h2>Find the best professionals for your project!</h2>
+    </main>
+  );
+};
+
+export default Home;
